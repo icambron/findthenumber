1d8d09577b886c670b35363a9e375ea343ba5d43
Isaac Cambron <icambron@gmail.com>
2012-11-13 06:01:43 -0500

    removing problem description from the code

--- FindTheNumber.cs
+++ FindTheNumber.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Pair = System.Tuple<int, int>;
 
-/*
-A man comes up with two numbers between 1 and 99 (meaning not 1 or 99) and gives one person the sum of these numbers and another person the product of
-these numbers. The person with the product comes up to the person with the sum and says "I don't know the two numbers". The person with the
-sum replies with "I know that you don't know the two numbers".  Immediately, the man with the product says "now I know the two
-numbers" and the man with the sum says, "now i know the two numbers too". What are the two numbers?
-*/
-
 namespace FindTheNumber
 {
   class FindTheNumber
